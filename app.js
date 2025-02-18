77a26ab4ac3a4e4df5b3b7a02b1dd8881f88a2ef
d-hooper <dallinhoop3@gmail.com>
2025-02-18 14:12:24 -0700

    added starter html and css for flavors/toppings cards and flavor objects

--- app.js
+++ app.js
@@ -1,5 +1,61 @@
 // SECTION Global variables
+const iceCream = [
+  {
+    name: 'Cookie Dough',
+    price: 1.25,
+    quantity: 0,
+    type: 'scoop'
+  },
+  {
+    name: 'Vanilla',
+    price: 1,
+    quantity: 0,
+    type: 'scoop'
+  },
+  {
+    name: 'Strawberry',
+    price: 1.25,
+    quantity: 0,
+    type: 'scoop'
+  },
+  {
+    name: 'Mint Chocolate Chip',
+    price: 1.25,
+    quantity: 0,
+    type: 'scoop'
+  },
+  {
+    name: 'Chocolate',
+    price: 1.25,
+    quantity: 0,
+    type: 'scoop'
+  },
+  {
+    name: 'Caramel',
+    price: 1.25,
+    quantity: 0,
+    type: 'scoop'
+  },
 
+  {
+    name: 'Sprinkles',
+    price: 0.25,
+    quantity: 0,
+    type: 'topping'
+  },
+  {
+    name: 'Chocolate Chips',
+    price: 0.25,
+    quantity: 0,
+    type: 'topping'
+  },
+  {
+    name: 'Cookie Chunks',
+    price: 0.50,
+    quantity: 0,
+    type: 'topping'
+  }
+]
 // !SECTION
 
 // SECTION Logic
